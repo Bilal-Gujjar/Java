44be8bf148053bd85a077ce0aba9a06b61af68ae
bilal-gujjar <bilaltahir515@gmail.com>
2022-03-21 18:24:06 +0500

    add liner

---
+++ BinarySearch.java
@@ -0,0 +1,10 @@
+package com.Bilal_Gujjar;
+
+public class Array {
+    public static void main(String[] args) {
+        System.out.println("Hellow");
+
+    }
+    sta
+}
+
---
+++ LinerSearch.java
@@ -0,0 +1,4 @@
+package com.Bilal_Gujjar;
+
+public class LinerSearch {
+}
